6841b702b88d05a22657fb5d5dc147586f3c9afd
qwertyRaketa572 <119349319+qwertyRaketa572@users.noreply.github.com>
2022-12-02 23:04:51 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,23 +3,31 @@ import random
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtGui import QColor, QPainter
 
 
 class MyWidget(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi('UI.ui', self)
-        self.pushButton.clicked.connect(self.run)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
 
-    def run(self):
-        self.pushButton.setHidden(True),
-        while True:
-            time.sleep(1000)
-            r = random.randrange(0, 600)
-            self.circle.move(random.randrange(0, 600), random.randrange(0, 600))
-            self.circle.resize(r, r)
-            self.label_1.setStyleSheet("border: 3px solid yellow;
-                                    border-radius: %dpx;" % r)
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_circle(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_circle(self, qp):
+        r = random.randrange(0, 600)
+        qp.setBrush(QColor(255, 255, 0))
+        qp.drawEllipse(random.randrange(0, 600), random.randrange(0, 600), r, r)
 
 
 if __name__ == '__main__':
